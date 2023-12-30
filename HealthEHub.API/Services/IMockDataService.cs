45ea95513a12c0462da4f17a6a8c04f5c04d3077
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-12-30 14:52:59 +0100

    Create mock data service & interface

---
+++ HealthEHub.API/Services/IMockDataService.cs
@@ -0,0 +1,11 @@
+﻿using SharedModels;
+namespace HealthEHub.API.Services
+{
+    public interface IMockDataService
+    {
+        IEnumerable<string> GetBodyParts();
+        IEnumerable<string> GetAllEquipment();
+        IEnumerable<string> GetTargetMuscles();
+        IEnumerable<Exercise> GetExercises();
+    }
+}
