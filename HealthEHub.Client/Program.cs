593d4a8b2d42e0551656acf1cc96b53c803f7181
Emil Helgesson <helgesson.emil.94@gmail.com>
2024-01-06 23:00:31 +0100

    Configure HttpClient to allow relative url paths & different paths depending on environment

--- HealthEHub.Client/Program.cs
+++ HealthEHub.Client/Program.cs
@@ -6,6 +6,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var environment = builder.HostEnvironment;
+
+builder.Services.AddScoped(sp =>
+{
+    string apiBaseUrl = environment.IsDevelopment()
+        ? "https://localhost:7024/"  // Local API for development
+        : "https://healthhubappapi.azurewebsites.net/";  // Deployed API for production
+
+    return new HttpClient { BaseAddress = new Uri(apiBaseUrl) };
+});
 
 await builder.Build().RunAsync();
+
