818d8858c8ce67d4617cabd8026775486fc03304
Emil Helgesson <helgesson.emil.94@gmail.com>
2024-01-13 16:06:33 +0100

    Add theme service

---
+++ HealthEHub.Client/Services/ThemeService.cs
@@ -0,0 +1,24 @@
+﻿namespace HealthEHub.Client.Services
+{
+    public class ThemeService
+    {
+        private bool _isDarkMode;
+        public bool IsDarkMode
+        {
+            get => _isDarkMode;
+            set
+            {
+                if (_isDarkMode != value)
+                {
+                    _isDarkMode = value;
+                    NotifyThemeChanged();
+                }
+            }
+        }
+
+        public event Action OnThemeChanged;
+
+        private void NotifyThemeChanged() => OnThemeChanged?.Invoke();
+    }
+
+}
