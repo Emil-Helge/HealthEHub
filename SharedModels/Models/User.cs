ee5632adfe24ccbd534f2468e5694f2cde13ac23
Emil Helgesson <helgesson.emil.94@gmail.com>
2024-01-17 08:25:45 +0100

    Change user model

--- SharedModels/Models/User.cs
+++ SharedModels/Models/User.cs
@@ -3,10 +3,10 @@
     public class User
     {
         public int UserId { get; set; }
-        public string GoogleId { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
         public List<WorkoutPlan> WorkoutPlans { get; set; } = [];
     }
-
 }
