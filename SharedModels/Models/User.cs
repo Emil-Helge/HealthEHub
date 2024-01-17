4bdf1911eededd8c683f3e523c365a4e09354452
Emil Helgesson <helgesson.emil.94@gmail.com>
2024-01-17 03:36:46 +0100

    Add models for user and workout plan

---
+++ SharedModels/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace SharedModels.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string GoogleId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public List<WorkoutPlan> WorkoutPlans { get; set; } = [];
+    }
+
+}
---
+++ SharedModels/Models/WorkoutPlan.cs
@@ -0,0 +1,12 @@
+﻿namespace SharedModels.Models
+{
+    public class WorkoutPlan
+    {
+        public int WorkoutPlanId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<Exercise> Exercises { get; set; } = [];
+        public int UserId { get; set; }
+        public User User { get; set; } = new User();
+    }
+
+}
