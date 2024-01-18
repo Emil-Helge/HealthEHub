5608efee6541ac08b2822c81b7c831cc1a6a16e2
Emil Helgesson <helgesson.emil.94@gmail.com>
2024-01-18 19:27:05 +0100

    Snackbar config

--- HealthEHub.Client/Program.cs
+++ HealthEHub.Client/Program.cs
@@ -2,6 +2,7 @@ using HealthEHub.Client;
 using HealthEHub.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -20,7 +21,18 @@ builder.Services.AddScoped(sp =>
 });
 
 builder.Services.AddScoped<AuthenticationService>();
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 builder.Services.AddScoped<ThemeService>();
 
 await builder.Build().RunAsync();
